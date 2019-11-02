3a8422a6631d93faaf63672b502cb2616b63f5ae
Ayriel Ash <aya37278@bethel.edu>
2019-11-02 00:08:57 -0500

    add directions and few fixes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,9 +10,34 @@ import Review from '../Review/Review';
 
 
 class App extends Component {
+  //set text for ranking feeling displays
   understand='How well do you feel you understand the content this week?';
   feel='How are you feeling this week?';
   support='How supported are you feeling?';
+
+  //set directions for routes
+  direction={
+    understanding:{
+      f: '/feeling',
+      b: '/',
+    },
+    feeling:{
+      f: '/support',
+      b: '/understanding',
+    },
+    support:{
+      f:'/comment',
+      b: '/feeling',
+    },
+    comment:{
+      f: '/review',
+      b: '/support',
+    },
+    review:{
+      f: '/',
+      b: '/comment'
+    },
+  }
 
   render() {
     return (
@@ -42,11 +67,18 @@ class App extends Component {
               <Link to="/review">Review</Link>
             </li>
           </ul>
-          <Route exact path="/feeling" render={()=><Rank action='SET_FEELING' stringContent={this.feeling} default={this.props.feeling}/>}/>
+          <Route exact path="/feeling" render={()=><Rank action='SET_FEELING' stringContent={this.feel} default={this.props.feeling}/>}/>
           <Route exact path="/understanding" render={()=><Rank action='SET_UNDERSTANDING' stringContent={this.understand} default={this.props.understanding}/>}/>
-          <Route exact path="/support" render={()=><Rank action='SET_SUPPORT' stringContent={this.support} default={this.props.support}/>}/>
-          <Route exact path="/comments" render={()=><Comments default={this.props.comments}/>}/>
-          <Route exact path="/review" component={Review}/>
+          <Route exact path="/support" 
+          render={()=>
+          <Rank action='SET_SUPPORT' 
+          stringContent={this.support} 
+          default={this.props.support}
+          direction={this.direction.support}/>}/>
+          <Route exact path="/comments" 
+          render={()=>
+          <Comments default={this.props.comments} direction={this.direction.comments}/>}/>
+          <Route exact path="/review" render={()=><Review direction={this.direction.review}/>}/>
           </Router>
         <pre>{JSON.stringify(this.props, null, 2)}</pre>
       </div>
