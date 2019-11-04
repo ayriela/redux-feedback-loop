4d1b77d33d91349dd3e6516c4481f6f04595a9e8
Ayriel Ash <aya37278@bethel.edu>
2019-11-04 09:48:21 -0600

    fix material ui links

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 //material ui formatting
-import Card from "@material-ui/core/Card";
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import Card from "material-ui/core/Card";
+import Button from 'material-ui/core/Button';
+import Typography from 'material-ui/core/Typography';
+import Dialog from 'material-ui/core/Dialog';
+import DialogActions from 'material-ui/core/DialogActions';
+import DialogContent from 'material-ui/core/DialogContent';
+import DialogContentText from 'material-ui/core/DialogContentText';
+import DialogTitle from 'material-ui/core/DialogTitle';
 //bring in routing
 import { withRouter } from 'react-router-dom';
 
