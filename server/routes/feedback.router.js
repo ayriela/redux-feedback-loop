d727b7ec94a9716cff1fa760d37e62cc077d399a
Ayriel Ash <aya37278@bethel.edu>
2019-11-04 09:04:44 -0600

    remove unneeded comments

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -2,18 +2,6 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
-/* // GET all orders that have been placed, populate with data from the pizza collection
-router.get('/', (req, res) => {
-    // Find all orders and return them
-    pool.query('SELECT * FROM "orders";').then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log('Error GET /api/order', error);
-        res.sendStatus(500);  
-    });
-})
- */
-
 //POST FEEDBACK
 router.post('/submit/',  (req, res) => {
     let newFeedback = req.body;
@@ -32,14 +20,5 @@ router.post('/submit/',  (req, res) => {
       });
   });
 
-/* // DELETE an order
-router.delete('/:id', (req, res) => {
-    pool.query('DELETE FROM "orders" WHERE id=$1', [req.params.id]).then((result) => {
-        res.sendStatus(200);
-    }).catch((error) => {
-        console.log('Error DELETE /api/order', error);
-        res.sendStatus(500);
-    })
-}); */
 
 module.exports = router;
